57bcc507d8476b9030a71bdd3fa49ea5ab014de0
Uneeck <urbantomasz87@gmail.com>
2017-02-23 11:27:16 +0100

    initial

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Podaj podstawę trójkąta: a =');
+var h = prompt('Podaj wysokość trójkąta: h =');
+var triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
